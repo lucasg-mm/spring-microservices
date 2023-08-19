5a1d071a74b9c70de39da6a1b2d01fe83941fd91
Lucas Miranda <lucasalternativo.lg@gmail.com>
2023-08-19 14:11:51 -0300

    Add exception handling to employee service

--- employee-service/src/main/java/com/lucas/employeeservice/exception/ResourceNotFoundException.java
+++ employee-service/src/main/java/com/lucas/employeeservice/exception/ResourceNotFoundException.java
@@ -1,10 +1,10 @@
-package com.lucas.departmentservice.exception;
+package com.lucas.employeeservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
     private String fieldValue;
--- employee-service/src/main/java/com/lucas/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/lucas/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,14 @@ package com.lucas.employeeservice.service.impl;
 
 import com.lucas.employeeservice.dto.EmployeeDto;
 import com.lucas.employeeservice.entity.Employee;
+import com.lucas.employeeservice.exception.ResourceNotFoundException;
 import com.lucas.employeeservice.mapper.EmployeeMapper;
 import com.lucas.employeeservice.repository.EmployeeRepository;
 import com.lucas.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.lang.module.ResolutionException;
 
 @AllArgsConstructor
 @Service
@@ -26,7 +29,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
-        Employee employee = employeeRepository.findById(employeeId).get();
+        Employee employee = employeeRepository
+                .findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId.toString()));
 
         return EmployeeMapper.MAPPER.mapToEmployeeDto(employee);
     }
